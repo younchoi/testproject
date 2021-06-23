90a7cb52fe62e0f4469ba0762741b3eb61ae403a
Ellie <ipatlove3@gmail.com>
2021-06-24 00:17:01 +0900

    Initial commit

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+def complicate_function(a):
+    a += 1000
+    a *= 0.5
+    """ 
+    very compicated
+     """
+
+     #a = "string"
+     a *= 0.1
+    return a
+
+if __name__ == "__main__":
+    a = 40
+    ret = complicate_function(a)
